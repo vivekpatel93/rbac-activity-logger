784c72dd8ed591e6e59ce6af178f36e8d3a43d20
Vivek patel <patelvivek.v2005@gmail.com>
2026-02-13 13:21:40 +0530

    created rbac with good folder structure.

---
+++ src/main/java/com/vivek/RbacActivityLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.vivek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacActivityLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacActivityLoggerApplication.class, args);
+	}
+
+}
